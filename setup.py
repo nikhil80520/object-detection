7625f568e0dfac9b7bb90646dc6c43538a63e9ba
nikhil80520 <msd23004@iiitl.ac.in>
2024-10-03 00:42:47 +0530

    Objection detection initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='signlanguages',
+    version='0.0.0',
+    author='Nikhil kumar',
+    author_email='nikhilkumar24802@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
